f8ad2361c1b53b8da0ea538aa8d204eaeb50e9c9
warthmann <norman@warthmann.com>
2021-02-19 19:20:57 +0100

    sorted needs a genome file

--- count_coverage.sh
+++ count_coverage.sh
@@ -4,24 +4,30 @@
 #mkdir -p output/500kb
 #mkdir -p output/10kb
 
-#bedtools makewindows -g Banana_chromosome_file -w 10000 > output/window_file_10kb
-#bedtools makewindows -g Banana_chromosome_file -w 100000 > output/window_file_100kb
-#bedtools makewindows -g Banana_chromosome_file -w 500000 > output/window_file_500kb
+#bedtools makewindows -g Banana_chromosome_file.tab -w 10000 > output/window_file_10kb
+#bedtools makewindows -g Banana_chromosome_file.tab -w 100000 > output/window_file_100kb
+#bedtools makewindows -g Banana_chromosome_file.tab -w 500000 > output/window_file_500kb
 
 cat list_of_banana_samplenames | \
 parallel -j 1 \
-bedtools coverage -sorted -a output/window_file_100kb \
+bedtools coverage -sorted \
+-a output/window_file_100kb \
 -b ~/data/banana/TR4_SEQ2020/Analysis_Feb2021/dna-proto-workflow/output/alignments/samples/bwa/GCF_000313855.2_ASM31385v2_genomic_plus_chloroplast/{}.bam \
+-g Banana_chromosome_file.tab \
 \> output/100kb/coverage_100kb_{}.out
 
 cat list_of_banana_samplenames | \
 parallel -j 1 \
-bedtools coverage -sorted -a output/window_file_500kb \
+bedtools coverage -sorted \
+-a output/window_file_500kb \
 -b ~/data/banana/TR4_SEQ2020/Analysis_Feb2021/dna-proto-workflow/output/alignments/samples/bwa/GCF_000313855.2_ASM31385v2_genomic_plus_chloroplast/{}.bam \
+-g Banana_chromosome_file.tab \
 \> output/500kb/coverage_500kb_{}.out
 
 cat list_of_banana_samplenames | \
 parallel -j 1 \
-bedtools coverage -sorted -a output/window_file_10kb \
+bedtools coverage -sorted \
+-a output/window_file_10kb \
 -b ~/data/banana/TR4_SEQ2020/Analysis_Feb2021/dna-proto-workflow/output/alignments/samples/bwa/GCF_000313855.2_ASM31385v2_genomic_plus_chloroplast/{}.bam \
+-g Banana_chromosome_file.tab \
 \> output/10kb/coverage_10kb_{}.out
